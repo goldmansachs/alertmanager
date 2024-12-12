30290353d06a0eaf81303ab1c00454dc5ee4fa4b
Moyo George <ThatOneAwkwardGuy@users.noreply.github.com>
2024-12-12 14:24:08 +0000

    Update Updates.elm

--- ui/app/src/Updates.elm
+++ ui/app/src/Updates.elm
@@ -48,7 +48,7 @@ update msg ({ basePath, apiUrl } as model) =
             )
 
         NavigateToSilenceFormNew params ->
-            ( model, if model.query /= "" then Navigation.load ("https://" ++ (if String.contains "qa." model.apiUrl then "preprod." else "") ++ "airlock.sre.gs.com/alertmanager/silence?" ++ model.query) else Cmd.none)
+            ( model, Navigation.load ("https://" ++ (if String.contains "qa." model.apiUrl then "preprod." else "") ++ "airlock.sre.gs.com/alertmanager/silence?" ++ model.query) )
 
         NavigateToSilenceFormEdit uuid ->
             ( { model | route = SilenceFormEditRoute uuid }, Task.perform identity (Task.succeed <| (FetchSilence uuid |> MsgForSilenceForm)) )
